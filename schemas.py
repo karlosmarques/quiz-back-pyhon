eb0d0896f95c5af3479504e330f7475a4e3e5c96
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-07-02 10:58:38 -0300

    adicionado  from_attributes = True

--- schemas.py
+++ schemas.py
@@ -18,20 +18,34 @@ class loginSchema(BaseModel):
 
 class Quizalternativas(BaseModel):
     texto: str
-    correta: bool
+    
+    class Config:
+        from_attributes = True
 
 class QuizPerguntas(BaseModel):
     texto: str
     opcoes: List[Quizalternativas]
 
+    class Config:
+        from_attributes = True
+
 class Quiztitulo(BaseModel):
     titulo: str
     perguntas: List[QuizPerguntas]
+
+    class Config:
+        from_attributes = True
 
 class RespostaItem(BaseModel):
     question_id: int
     answer_id: int
 
+    class Config:
+        from_attributes = True
+
 class ResponderQuizSchema(BaseModel):
     quiz_id: int
     respostas: List[RespostaItem]
+
+    class Config:
+        from_attributes = True
